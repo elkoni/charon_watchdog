93a2c33a85e527bd77b68193363037df4ee3b51b
elkoni <fich16@abv.bg>
2025-03-03 08:05:06 +0200

    Create charon_wd.sh

---
+++ charon_wd.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Charon daemon watchdog script - charon-systemd required
+# responder has a dynamic ip and static FQDN ( dynamic DNS for example )
+# responder ip can change after ISP connection drops and reestablishesA
+# charon does not re-resolve peer when DPD or retransmit is active
+# ver 0.0.0.1
+#
+# This script needs permissions to /var/run/charon.vici socket
+REMOTE=my.remote.fqdn
+SWANOUT=$(swanctl --list-sas 2>/dev/null)
+ERR=$?
+if [ ${ERR}  -ne 0 ];
+        then
+                echo "Swanctl failed whith error ${ERR}"
+                exit
+fi
+ACTIP=$(echo "${SWANOUT}" | awk -F[\@\[\ ] '/remote/{print $7;exit}')
+if [ -z ${ACTIP} ];
+        then
+                echo "Remote IKE info not available"
+                exit
+fi
+NEWIP=$(dig +short ${REMOTE})
+if [ -z ${NEWIP} ];
+        then
+                echo "Cant resolve remote FQDN"
+                exit
+fi
+
+# debug info
+[[ ${ACTIP}x == ${NEWIP}x ]] && echo ${ACTIP} ${NEWIP}
+if [ ${ACTIP}x != ${NEWIP}x ];
+        then
+                echo "Restarting charon daemon to re-resolve remote"
+                systemctl restart strongswan.service
+fi
